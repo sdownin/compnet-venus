24e871fe7b44c1c8e86911287d9092a7ba23cf89
Stephen Downing <stephendowning2008@gmail.com>
2016-10-24 13:12:08 +0800

    adds ArgParser

--- python/crunchbase.py
+++ python/crunchbase.py
@@ -110,14 +110,21 @@ class CrunchBase(object):
     
 ##--------------------------------------------------------
 if __name__ == '__main__':
-#    relations = ['ipo','products']
-#    companies = ['google','dropbox']
-#    cb = CrunchBase()
-#    cb.getPaginatedResults(companies, relations)
-    os.chdir('C:/Users/sdowning/Google Drive/PhD/Dissertation/crunchbase')
-    relations = ['ipo','products','owned_by','sub_organizations','investors','investments','board_members_and_advisors']     ## ['competitors']  # 'members','memberships'
-    co_names =  pd.read_csv('g_full_company_name_unique.csv',encoding='utf-8')
-    companies = co_names.name.values
-    start_index = 6799  # 0
-    cb = CrunchBase()
-    cb.getPaginatedResults(companies[start_index:], relations)
+    from argparse import ArgumentParser
+    par = ArgumentParser(description="CrunchBase API Calls")
+    par.add_argument('user_key', type=str, help="CrunchBase API user_key") 
+    par.add_argument('--companies', type=str, default='google', help="The company entities to request (comma-separated) [default 'google']")
+    par.add_argument('--relations', type=str, default='products', help="The relations to request (comma-separated) [default 'products']")
+    args = par.parse_args()
+    user_key = args.user_key
+    companies = args.companies.split(',') if ',' in args.companies else [args.companies]
+    relations = args.relations.split(',') if ',' in args.relations else [args.relations]
+    ##-------------------
+#    os.chdir('C:/Users/sdowning/Google Drive/PhD/Dissertation/crunchbase')
+#    relations = ['ipo','products','owned_by','sub_organizations','investors','investments','board_members_and_advisors']     ## ['competitors']  # 'members','memberships'
+#    co_names =  pd.read_csv('g_full_company_name_unique.csv',encoding='utf-8')
+#    start_index = 6799  # 0
+#    companies = co_names.name.values[start_index:]
+    ##-------------------
+    cb = CrunchBase(user_key)
+    cb.getPaginatedResults(companies, relations)
