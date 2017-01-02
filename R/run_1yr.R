8cdd08940e527a7dbb6723ac08f07e142765e9f4
Stephen Downing <stephendowning2008@gmail.com>
2017-01-02 15:36:42 +0800

    adds results

--- R/run_1yr.R
+++ R/run_1yr.R
@@ -11,9 +11,10 @@ mmc <- lapply(nets.sub, function(net) as.matrix(net %n% 'mmc'))
 sim <- lapply(nets.sub, function(net) as.matrix(net %n% 'similarity'))
 
 cat('beginning tergm computation')
-cat(sprintf('detected %s cores',detectCores()))
-
-ncpus <- 4
+ncores <- detectCores()
+cat(sprintf('detected %s cores',ncores))
+ncpus <- ifelse(ncores > 16, 16, ncores)
+cat(sprintf('using %s cpus', ncpus))
 
 fb1 <- btergm(nets.sub ~ edges + gwesp(0, fixed=T)  +
               nodefactor('state_code') + nodematch('state_code', diff=F) +
@@ -26,6 +27,13 @@ fb1 <- btergm(nets.sub ~ edges + gwesp(0, fixed=T)  +
               cycle(3) + cycle(4) + cycle(5)
             , R=10 ,  parallel = "multicore", ncpus = ncpus)
 
+btergm::btergm.se(fb1, print=T)
+
+cat('completed btergm fit. running diagnostics...\n')
+(g1 <- btegm::gof(fb1))
+
 cat('saving image\n')
 save.image('run_nrd_1y_1.RData')
+
 cat('completed successfully.\n')
+
